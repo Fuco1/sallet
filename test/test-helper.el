c2c3c75172b48c33a622faf1386b9b09d65c56dc
Matus Goljer <matus.goljer@gmail.com>
2016-05-14 19:53:30 +0200

    Rename test to tests

--- test/test-helper.el
+++
@@ -1,3 +0,0 @@
-(let ((package-dir (f-parent (f-dirname (f-this-file)))))
-  (add-to-list 'load-path package-dir))
-(require 'sallet)
---
+++ tests/sallet-start-process-test.el
@@ -0,0 +1,23 @@
+ (require 'sallet)
+
+(describe "Process helpers"
+  (it "should construct argument lists"
+
+    (expect
+     (sallet-process-args (list "grep" "-n" (when t (list "-E" "foo")) "this.el"))
+     :to-equal
+      (list "grep" "-n" "-E" "foo" "this.el"))
+
+    (expect
+     (sallet-process-args (list "grep" "-n" (when nil (list "-E" "foo")) "this.el"))
+     :to-equal
+      (list "grep" "-n" "this.el"))
+    (expect
+     (sallet-process-args (list "grep" "-n" "this.el"))
+     :to-equal
+      (list "grep" "-n" "this.el"))
+
+    (expect
+     (sallet-process-args (list "grep" "-n" (when t "foo") "this.el"))
+     :to-equal
+      (list "grep" "-n" "foo" "this.el"))))
