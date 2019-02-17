a9775a8bb7190554be45eaabd43bf6f261000e9b
Matus Goljer <goljer@logio.cz>
2019-02-17 16:12:39 +0100

    chore: add pkg-file

---
+++ sallet-pkg.el
@@ -0,0 +1,9 @@
+(define-package "sallet" "0.0.1" "Light spherical helmet."
+  '((cl-lib "0.3")
+    (ov "1.0")
+    (shut-up "0.3.2")
+    (async "1.2")
+    (flx "0.4")
+    (f "0.18.2")
+    (s "1.9.0")
+    (dash "2.10.0")))
